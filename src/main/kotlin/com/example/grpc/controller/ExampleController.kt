f42363d3b9a1d8b204caca280b7d440d9d835e9e
Vasanth Gopal <vasanth.gopal0@loblaw.ca>
2024-04-27 18:24:08 -0400

    Use service in controller

--- src/main/kotlin/com/example/grpc/controller/ExampleController.kt
+++ src/main/kotlin/com/example/grpc/controller/ExampleController.kt
@@ -2,21 +2,23 @@ package com.example.grpc.controller
 
 import com.example.grpc.proto.Example.ExampleMessage
 import com.example.grpc.repository.ExampleRepository
+import com.example.grpc.service.ExampleService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api")
 class ExampleController(
-    private val exampleRepository: ExampleRepository,
+    private val exampleService: ExampleService,
 ) {
 
     @RequestMapping(value = ["/example/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getExample(@PathVariable id: String) : ExampleMessage {
-        return ExampleMessage.newBuilder().setId(id).setName("name").build()
+    fun getExample(@PathVariable id: String) : Mono<ExampleMessage> {
+        return exampleService.getExample(id)
     }
 
 }
